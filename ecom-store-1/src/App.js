e1be5db4189478f5cc2e054f284776f81bd49753
Sam Ardis <ardis.productions@gmail.com>
2021-06-30 15:27:48 -0400

    Added files, organized files, added CSS files and started coding out the main components as well as some styling.

--- ecom-store-1/src/App.js
+++ ecom-store-1/src/App.js
@@ -1,12 +1,75 @@
-import './App.css';
+import React, { useEffect } from "react";
+import "./CSS/App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Components/Checkout";
+import Login from "./Components/Login";
+import Payment from "./Components/Payment";
+import Orders from "./Components/Orders";
+import { auth } from "./Config/firebase";
+import { useStateValue } from "./Components/StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51J7sljL4B4zSIfLNxKIad28DJcFWSzNa3vqfiR1ZcHMk3tCNGKOWEsOcv3g7pkoSZA2BWkQxYV8xvMtRlnltSygO00nE4zuavJ"
+);
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div></div>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+  </Router>
   );
 }
 
--- ecom-store-1/src/index.js
+++ ecom-store-1/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './CSS/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer, { initialState } from "./Reducers/reducer";
+import { StateProvider } from "./Components/StateProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
