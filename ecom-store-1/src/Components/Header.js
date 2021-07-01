e50b992ff166248c0244d96610543a31b60d59f0
Sam Ardis <ardis.productions@gmail.com>
2021-07-01 16:41:21 -0400

    Added darkmode, restyled the nav, changed a couple styling elements, and created better astethics

--- ecom-store-1/src/Components/Header.js
+++ ecom-store-1/src/Components/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "../CSS/Header.css";
+import "../CSS/App.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 import { auth } from "../Config/firebase";
+import useDarkMode from "../Hooks/useDarkMode";
 
 function Header() {
   const [{ basket, user }, dispatch] = useStateValue();
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = event => {
+      event.preventDefault();
+      setDarkMode(!darkMode);
+  };
 
   const handleAuthenticaton = () => {
     if (user) {
@@ -17,27 +24,41 @@ function Header() {
 
   return (
     <div className="header">
-      <Link to="/">
-        <img
-          className="header__logo"
-          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        />
-      </Link>
 
-      <div className="header__search">
-        <input className="header__searchInput" type="text" />
-        <SearchIcon className="header__searchIcon" />
+        <div className= "header__topDiv">
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          />
+        </Link>
+
+        <div className="dark-mode__container">
+                    <span className="light__symbol">☀︎</span>
+                      <div className='dark-mode__toggle'>
+                          <div 
+                          onClick={toggleMode} 
+                          className={darkMode ? 'toggle toggled' : 'toggle'} 
+                          />
+                      </div>
+                  <span className="dark__symbol">☾</span>
+              </div>
+          </div>
+
+        <div className="header__search">
+          <input className="header__searchInput" type="text" />
+          <SearchIcon className="header__searchIcon" />
+
       </div>
-
       <div className="header__nav">
-        <Link to={!user && '/login'}>
+        <Link style={{textDecoration: 'none'}} to={!user && '/login'}>
           <div onClick={handleAuthenticaton} className="header__option">
             <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
             <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
           </div>
         </Link>
 
-        <Link to='/orders'>
+        <Link style={{textDecoration: 'none'}} to='/orders'>
           <div className="header__option">
             <span className="header__optionLineOne">Returns</span>
             <span className="header__optionLineTwo">& Orders</span>
@@ -45,7 +66,7 @@ function Header() {
         </Link>
         
 
-        <div className="header__option">
+        <div style={{textDecoration: 'none'}} className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
